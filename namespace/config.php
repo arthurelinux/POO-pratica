2eb5025825f791e22e1aacb033bb065fab96e28e
Renan <renanoliveira1988@gmail.com>
2020-12-16 18:00:17 -0300

    Update config.php

--- namespace/config.php
+++ namespace/config.php
@@ -4,7 +4,8 @@ spl_autoload_register(function($nameClass){
 var_dump($nameClass);
 
 $dirClass ="class";
-$FileName = $dirClass.DIRECTORY_SEPARATOR.$nameClass.".php";
+//$FileName = $dirClass.DIRECTORY_SEPARATOR.$nameClass.".php";
+$FileName = __DIR__. DIRECTORY_SEPARATOR.$dirClass.DIRECTORY_SEPARATOR.$nameClass.".php";
 
 if(file_exists($FileName)){
     include($FileName);
